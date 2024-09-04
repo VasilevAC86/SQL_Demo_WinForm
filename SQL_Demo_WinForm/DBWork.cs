4f06cd990f3f7990426eee28321e65885492e9d7
Top <Top@WIN-7RSES2G9CV4>
2024-09-04 21:16:12 +0500

    Добавьте файлы проекта.

---
+++ SQL_Demo_WinForm/DBWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite; // Добавляем библиотеку
+
+namespace SQL_Demo_WinForm
+{
+    internal class DBWork
+    {
+        static public string MakeDB(string _dbname = "test02")
+        {
+            string result = "Ошибка чтения данных ...";
+            string path = $"Data Source={_dbname};"; // Путь для подключения к БД
+            // Переменная для sql-запросов (таблица запросов)
+            string init_db = "CREATE TABLE IF NOT EXISTS " + // Создаём таблицу, если она не существует
+                "Category " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "Name VARCHAR);";
+            string init_data = "INSERT INTO " + // Заполняем таблицу данными
+                "Category" +
+                "(Name)" +
+                "VALUES" +
+                "('SportwatchWatch');";
+            string showAllData = "SELECT * FROM Category;"; // Переменная для просмотра данных
+            SQLiteConnection conn = new SQLiteConnection(path);
+            // Создание команд, которые мы будем последовательно выполнять
+            SQLiteCommand cmd01 = conn.CreateCommand();
+            SQLiteCommand cmd02 = conn.CreateCommand();
+            SQLiteCommand cmd03 = conn.CreateCommand();
+            cmd01.CommandText = init_db; // Инициализируем базу данных
+            cmd02.CommandText = init_data; // Заполнение данных
+            cmd03.CommandText = showAllData; // Вернёт что-то или ничего
+            conn.Open(); // Открытие соединения с БД
+            cmd01.ExecuteNonQuery(); // Наш запрос выполнится, но его результат успех или неуспех
+            cmd02.ExecuteNonQuery(); // Возвращет кол-во изменённых строк            
+            var reader = cmd03.ExecuteReader(); // Будет возвращать System.object, который можно изменять
+            if (reader.HasRows) // Если объект имеет поля
+            {
+                result = " "; // Обнуляем результат
+                // reader.FieldCount // Если в while делать цикл for
+                while (reader.Read()) // Пока идёт чтение таблицы
+                {
+                    result += reader.GetValue(0).ToString();
+                    result += " | "; // Разделитель
+                    result += reader.GetValue(1).ToString();
+                    result += "\n"; // Когда прочитал всю строку
+                }
+            }
+            conn.Close(); // Закрытие соединения с БД
+            return result;
+        }
+    }
+}
---
+++ SQL_Demo_WinForm/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace SQL_Demo_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOutput.Location = new System.Drawing.Point(53, 54);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(434, 127);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRefresh.Location = new System.Drawing.Point(562, 54);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(185, 70);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Обновить";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.tbOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
